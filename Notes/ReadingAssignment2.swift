64799c2ddc2e8a89a7abf1f184d834e4880dedcb
dougwells <dougwells@gmail.com>
2017-03-16 08:02:28 -0600

    Reading Assignment 2. Numeric Literals & Type Aliases

---
+++ Notes/ReadingAssignment2.swift
@@ -0,0 +1,33 @@
+//
+//  ReadingAssignment2.swift
+//  Calculator
+//
+//  Created by Doug Wells on 3/16/17.
+//  Copyright © 2017 Doug Wells. All rights reserved.
+//
+
+/*
+ 
+ Type Aliases
+ Type aliases define an alternative name for an existing type.
+ refer to an existing type by a name that is descriptive
+ typealias AudioSample = UInt16
+ var maxAmplitudeFound = AudioSample.min
+ Because it is an alias, the call to AudioSample.minactually calls UInt16.min
+
+ Numeric Literals
+ Represent numbers in different ways (1.25e2, binary, hexidec, etc)
+ Numbers can contain extra formatting to make them easier to read
+ padded with extra zeros and underscores for readability
+ let paddedDouble = 000123.456
+ let oneMillion = 1_000_000
+ let justOverOneMillion = 1_000_000.000_000_1
+ 
+ Converting number types
+ Float/Double to Integer
+ Int is simply truncated
+ 4.75 becomes 4, and -3.9 becomes -3
+ Int(Double.pi)  // 3
+
+ 
+*/
