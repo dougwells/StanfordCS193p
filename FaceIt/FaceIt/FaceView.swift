91e837dc1f43da6c4401c30ae9381e6aa9ef8c35
dougwells <dougwells@gmail.com>
2017-03-19 11:42:43 -0700

    Lecture 4. Draw Face. Add eyes (& boolean for eyes open/close)

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -11,14 +11,71 @@ import UIKit
 class FaceView: UIView {
 
     var scale: CGFloat = 0.9
+    var eyesOpen = false
+    
+    private var skullRadius: CGFloat {
+        get {
+            return min(bounds.size.width, bounds.size.height) / 2 * scale
+        }
+    }
+    
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    enum Eye {
+        case left
+        case right
+    }
+    
+    private func pathForEye(_ eye: Eye) -> UIBezierPath {
+        func centerOfEye(_ eye: Eye) -> CGPoint {
+            let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
+            var eyeCenter = skullCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+            return eyeCenter
+        }
+        
+        let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
+        let eyeCenter = centerOfEye(eye)
+        
+        let path: UIBezierPath
+        
+        if eyesOpen {
+            path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        } else {
+            path = UIBezierPath()
+            path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
+            path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
+        }
+        
 
-    override func draw(_ rect: CGRect) {
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2 * scale
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        path.lineWidth = 5.0
+        return path
+        
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: false)
         path.lineWidth = 5.0  //5 points (not pixels) wide
+        return path
+    }
+
+    override func draw(_ rect: CGRect) {
         UIColor.blue.set()
-        path.stroke()
+        pathForSkull().stroke()
+        pathForEye(.left).stroke()
+        pathForEye(.right).stroke()
+    }
+    
+    private struct Ratios {
+        static let skullRadiusToEyeOffset: CGFloat = 3
+        static let skullRadiusToEyeRadius: CGFloat = 10
+        static let skullRadiusToMouthWidth: CGFloat = 1
+        static let skullRadiusToMouthHeight: CGFloat = 3
+        static let skullRadiusToMouthOffset: CGFloat = 3
+        
     }
 
 }
