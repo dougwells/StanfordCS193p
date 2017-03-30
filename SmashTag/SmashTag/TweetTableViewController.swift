4992c78551d384bad614655defd314c5671801e0
dougwells <dougwells@gmail.com>
2017-03-30 12:46:26 -0600

    L9. SmashTag. Add Twitter API via workspace.

--- SmashTag/SmashTag/TweetTableViewController.swift
+++ SmashTag/SmashTag/TweetTableViewController.swift
@@ -7,11 +7,53 @@
 //
 
 import UIKit
+import Twitter
+
 
 class TweetTableViewController: UITableViewController {
 
-    private var tweets = [Array<Tweet>]()
+    private var tweets = [Array<Twitter.Tweet>]() {
+        didSet {
+            print(tweets)
+        }
+    }
     
+    //computed variable
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            tableView.reloadData()  //The "hammer" but data removed so OK
+            searchForTweets()
+            title = searchText
+        }
+    }
+    
+    private func twitterRequest() -> Twitter.Request? {
+        if let query = searchText, !query.isEmpty {
+            return Twitter.Request(search: query, count: 100)
+        }
+        return nil
+    }
+    
+    private var lastTwitterRequest: Twitter.Request?
+    
+    private func searchForTweets() {
+        if let request = twitterRequest() {
+            let lastTwitterRequest = request
+            
+            request.fetchTweets { [weak self] newTweets in
+                if request == self?.lastTwitterRequest {
+                    self?.tweets.insert(newTweets, at: 0) //add to beginning of tweats arr
+                }
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchText = "#stanford"
+    }
     
     // MARK: - Key UITableViewDataSource methods
 
