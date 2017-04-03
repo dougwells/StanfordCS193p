01f1fe239103d80be7d5c149e98265275479b785
dougwells <dougwells@gmail.com>
2017-04-03 06:20:05 -0600

    L10. Notes. Add notes for first half of L10 Lecture.

---
+++ Notes/L10 Notes - Core Data.swift
@@ -0,0 +1,28 @@
+//
+//  L10 Notes - Core Data.swift
+//  
+//
+//  Created by Doug Wells on 4/3/17.
+//
+//
+
+/*
+ Core Data
+ 
+ Set up Model in storyboard like interface
+ Add Entities (like classes)
+ Add Attributes (like vars/prop w/in Entitites)
+ Remember to set Type
+ Create relationships (each side)
+ 
+ 
+ How access (insert, delete, edit, etc) the model?
+ Need the var viewContext from NSManagedObjectContext
+ get this from NSPersistantContainer
+ (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+
+ 
+ 
+ 
+ 
+*/
