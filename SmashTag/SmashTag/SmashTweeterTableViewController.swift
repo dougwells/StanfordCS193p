e173720a08d826357b7e74f45b7f03ee100395e1
dougwells <dougwells@gmail.com>
2017-04-08 15:57:47 -0600

    L11. Core Data. Add UITableView that list Twitter Users mentioning twitter search item

---
+++ SmashTag/SmashTag/SmashTweeterTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SmashTweeterTableViewController.swift
+//  SmashTag
+//
+//  Created by Doug Wells on 4/8/17.
+//  Copyright © 2017 Doug Wells. All rights reserved.
+//
+
+import UIKit
+
+class SmashTweeterTableViewController: UITableViewController
+{
+
+}
