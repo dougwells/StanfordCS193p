2518bc5005c80e14b374a134bbc545f84b27f876
dougwells <dougwells@gmail.com>
2017-04-09 10:06:21 -0600

    L11. Core Data. Add UITableView that shows tweeters & number of tweets they have sent on the searched term.

--- SmashTag/SmashTag/SmashTweetTableViewController.swift
+++ SmashTag/SmashTag/SmashTweetTableViewController.swift
@@ -48,4 +48,13 @@ class SmashTweetTableViewController: TweetTableViewController {
             }
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Tweeters Mentioning Search Term" {
+            if let tweetersTVC = segue.destination as? SmashTweetersTableViewController {
+                tweetersTVC.mention = searchText
+                tweetersTVC.container = container
+            }
+        }
+    }
 }
