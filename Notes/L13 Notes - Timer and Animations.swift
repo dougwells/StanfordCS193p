fd71a90059f1d00a93ddd2da5ef78ba8c9af3fb5
dougwells <dougwells@gmail.com>
2017-04-10 17:11:52 -0600

    L13 Notes. Timer & Animations. In depth on UIView Animations

--- Notes/L13 Notes - Timer and Animations.swift
+++ Notes/L13 Notes - Timer and Animations.swift
@@ -39,8 +39,77 @@
  SpriteKit - “2.5 D”
  Core Animation “CA” - powerful underlying animation framework
  Animating Controller Transitions (like UINavigationController)
- *Animating UIView Properties - change frame, transparency, etc
+ *UIView Animations - change frame, transparency, etc
  *Dynamic Animations - “Physics” based animations
+ 
+ 
+ UIView Animations
+ Done with UIView class methods that take closures
+ What can you animate?
+ frame/center		→ position (can slide view around screen)
+ transform		→ size/scale & rotation. translation = move
+ alpha			→ opacity
+ backgroundColor	→ color
+ 
+ 
+ Animations happen IMMEDIATELY in code.  Takes time on screen
+ 
+ 
+ Animations are methods on UIView → animate(withDuration: … )
+ class func animate(withDuration: 	TimeInterval,
+ delay:		TimeInterval,
+ options:	UIAnimationOptions,
+ animations: 	() -> Void,	//what you are animating
+ completion:	((finished: Bool) -> Void)?  //at anim done
+ )
+ 
+ 
+ Example
+ if myView.alpha == 1.0 {
+ UIView.animate(withDuration = 3.0,
+ delay: 2.0,
+ options: [.curveLinear]
+ animations: {myView.alpha = 0},
+ completion: {if $0 {myView.removeFromSuperView() } }
+ )
+ print("myView.alpha = \(myView.alpha)")
+ // 0 b/c code executes immediately
+ }			//not 0 on screen for 5 more seconds
+ 
+ 
+	Other Options:
+ Change entire View
+ UIViewAnimationOptions.transitionFlipFrom{Left, Right, Top, Bottom}
+ .transitionCrossDissolve
+ .transitionCurl{Up, Down}
+ 
+ UIView.transition(with: UIView,
+ duration: 	TimeInterval,
+ options:	UIViewAnimationOptions,
+ animations:	() -> Void,
+ completion: 	((finished) -> Void)?
+ )
+ 
+ Example:
+ //Flipping a playing card over:
+ UIView.transition(with: myPlayingCardView,
+ duration: 0.75,
+ options: [.transitionFlipFrom(Left)],
+ animations: {cardIsFaceUp = !cardIsFaceUp }
+ completion: nil
+ )
+ 
+ 
+ Change View hierarch (put elements into or take out of the view)
+ UIView.transition(from: 	UIView,		//transitions one to next
+ to:		UIView,
+ duration:	Timeinterval,
+ options:	UIViewAnimationOptions,
+ completion: 	((finished) -> Void)?
+ //UIViewAnimationOptions.showHideTransitionView or actually removed vs .isHidden
+ 
+ )
+
 
  
 */
