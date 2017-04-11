6fd3d18d9d99b5806274651040c80bd3270786be
dougwells <dougwells@gmail.com>
2017-04-11 16:15:10 -0600

    L14. Animations. Asteroid. Add collisions and boundaries. Next - eliminate friction.

--- Asteroids/Asteroids/AsteroidBehavior.swift
+++ Asteroids/Asteroids/AsteroidBehavior.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class AsteroidBehavior: UIDynamicBehavior
 {
+    
+    private lazy var collider: UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.collisionMode = .everything
+        behavior.translatesReferenceBoundsIntoBoundary = true  //sets edges of view to boundary
+        return behavior
+    }()
+    
+    override init() {
+        super.init()
+        addChildBehavior(collider)
+    }
+    
+    func addAsteroid(_ asteroid: AsteroidView){
+        asteroids.append(asteroid)
+        collider.addItem(asteroid)
+    }
+    
+    func removeAsteroid(_ asteroid: AsteroidView) {
+        if let index = asteroids.index(of: asteroid) {
+            asteroids.remove(at: index)
+        }
+        collider.removeItem(asteroid)
+    }
+    
+    
     func pushAllAsteroids(by magnitude: Range<CGFloat> = 0..<0.5){
         for asteroid in asteroids {
             let pusher = UIPushBehavior(items: [asteroid], mode: .instantaneous)
@@ -21,15 +47,6 @@ class AsteroidBehavior: UIDynamicBehavior
     
     private var asteroids = [AsteroidView]()
     
-    func addAsteroid(_ asteroid: AsteroidView){
-        asteroids.append(asteroid)
-    }
-    
-    func removeAsteroid(_ asteroid: AsteroidView) {
-        if let index = asteroids.index(of: asteroid) {
-            asteroids.remove(at: index)
-        }
-    }
-    
+   
 
 }
