e7949b15b203cd5202acc46d20ff567bce1ad58f
dougwells <dougwells@gmail.com>
2017-04-16 17:00:26 -0600

    L15 Notes. Modals and Segues.

---
+++ Notes/L15 Modals & Segues.swift
@@ -0,0 +1,18 @@
+//
+//  L15 Modals & Segues.swift
+//  FaceIt
+//
+//  Created by Doug Wells on 4/16/17.
+//  Copyright © 2017 Doug Wells. All rights reserved.
+//
+
+/*
+ 
+ --- Segueing from Modal to another MVC
+ Unwind Segues from modals
+ cntrl-drag to “exit” icon on top of current MVC. 2 requirements:
+ “Presenter of the modal, must have an @IBAction method
+ that @IBAction method can only have one argument which is UIStoryboardSegue
+
+ 
+*/
