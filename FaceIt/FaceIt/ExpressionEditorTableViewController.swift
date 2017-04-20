49b39f86a4e7328ab2d0178af483f1604ce08af2
dougwells <dougwells@gmail.com>
2017-04-20 08:29:40 -0600

    L15. Modals. Wire segment controller to set facial expression & resign first responder (hide keyboard) after enter name in text field.

--- FaceIt/FaceIt/ExpressionEditorTableViewController.swift
+++ FaceIt/FaceIt/ExpressionEditorTableViewController.swift
@@ -8,7 +8,33 @@
 
 import UIKit
 
-class ExpressionEditorTableViewController: UITableViewController {
+class ExpressionEditorTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    var name: String {
+        return nameTextField?.text ?? ""
+    }
+    
+    private let eyeChoices = [FacialExpression.Eyes.open, .closed, .squinting]
+    
+    private let mouthChoices = [FacialExpression.Mouth.frown, .smirk, .nuetral, .grin, .smile]
+    
+    
+    var expression: FacialExpression {
+        return FacialExpression(
+            eyes: eyeChoices[eyeControl?.selectedSegmentIndex ?? 0],
+            mouth: mouthChoices[mouthControl?.selectedSegmentIndex ?? 0])
+    }
+    
+    @IBAction func updateFace() {
+        print("name = \(expression)")
+    }
+
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var eyeControl: UISegmentedControl!
+    @IBOutlet weak var mouthControl: UISegmentedControl!
+    
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 1 {
@@ -16,6 +42,11 @@ class ExpressionEditorTableViewController: UITableViewController {
         } else {
             return super.tableView(tableView, heightForRowAt: indexPath)
         }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
     @IBAction func cancel(_ sender: UIBarButtonItem) {
