cdc33bd2ba9ce833b0a3c3808975389f33cf1910
dougwells <dougwells@gmail.com>
2017-04-20 07:42:47 -0600

    L15. Modals. Modify table view so first row (w/face) is square & add layout constraints.

--- FaceIt/FaceIt/ExpressionEditorTableViewController.swift
+++ FaceIt/FaceIt/ExpressionEditorTableViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ExpressionEditorTableViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 1 {
+            return tableView.bounds.width
+        } else {
+            return super.tableView(tableView, heightForRowAt: indexPath)
+        }
+    }
 
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         presentingViewController?.dismiss(animated: true)
