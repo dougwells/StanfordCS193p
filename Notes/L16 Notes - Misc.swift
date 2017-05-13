9d5b346a5c13341f62f3c45b2af26a27508f926d
dougwells <dougwells@gmail.com>
2017-05-12 21:48:26 -0600

    Notes. L16. Alerts and Action Sheets.

---
+++ Notes/L16 Notes - Misc.swift
@@ -0,0 +1,12 @@
+//  L16 Notes - Misc.swift
+//  Created by Doug Wells on 5/12/17
+
+/*
+ 
+ Alert - pop ups. Appear modally.  Usually yes/no OK/Cancel type questions
+ 
+ Action Sheets - slide up from bottom.  Usually 2+ questions.  Branching decisions.  What next?
+ 
+*/
+
+
