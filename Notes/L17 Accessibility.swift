d3aa675b2ac6542e5d3e472481b6676124ec9499
dougwells <dougwells@gmail.com>
2017-05-20 16:19:35 -0700

    Notes. L17. Accessibility. 5 main accessibility traits.

--- Notes/L17 Accessibility.swift
+++ Notes/L17 Accessibility.swift
@@ -12,6 +12,12 @@ import Foundation
  Guest lecturer from Apple's accessibility team
  
  UIAccessibility Protocol
+    5 Main Accessibility Traits
+        isAccessibilityElement: Bool
+        accessibilityLabel: String?
+        accessibilityTraits: UIAccessibilityTraits
+        accessibilityValue: String?
+        accessibilityHint: String?
  
  
 */
