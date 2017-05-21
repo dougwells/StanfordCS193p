bf182f650493dd1185b42e5e7be650798ea49c2f
dougwells <dougwells@gmail.com>
2017-05-21 13:13:25 -0700

    Notes. L17. Accessibility. Simple code example setting up accessibility.

--- Notes/L17 Accessibility.swift
+++ Notes/L17 Accessibility.swift
@@ -19,5 +19,23 @@ import Foundation
         accessibilityValue: String?
         accessibilityHint: String?
  
+    Example 1:
+        @IBOutlet weak var imageView: MyImageView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+ 
+            imageView.isAccessibilityElement = true
+            imageView.accessibilityLabel = "My Image Label"
+            imageView.accessibilityTraits = UIAccessibilityTraitImage
+        }
+ 
+    Better way to do example 1 above is with "get set" (w/all properties:)
+ 
+        override var isAccessibilityElement: Bool {
+            get {
+                return true
+            }
+ 
  
 */
